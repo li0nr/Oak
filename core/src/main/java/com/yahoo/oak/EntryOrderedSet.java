92907ac7ac710eaed1ddc6179b10ae0ae77a154b
Ramy Fakhoury <li0nr208@gmail.com>
2021-09-02 06:04:49 +0300

    introducing Nova MM

--- core/src/main/java/com/yahoo/oak/EntryOrderedSet.java
+++ core/src/main/java/com/yahoo/oak/EntryOrderedSet.java
@@ -319,6 +319,7 @@ class EntryOrderedSet<K, V> extends EntryArray<K, V> {
         for (int i = 0; i < getNumOfEntries(); i++) {
             if (valuesMemoryManager.isReferenceDeleted(getValueReference(i))) {
                 if (keysMemoryManager.isReferenceValidAndNotDeleted(getKeyReference(i))) {
+                    //TODO may add exception to isDelted and stop deleting if key is already deleted
                     key.s.decodeReference(getKeyReference(i));
                     if (key.s.isDeleted() != ValueResult.TRUE) {
                         if (key.s.logicalDelete() == ValueResult.TRUE) {
